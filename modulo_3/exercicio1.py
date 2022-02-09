c9961424b4e8a4e1999f1ad58442dfe7860a9f92
Leandro Watanabe <setsuo.leandro@gmail.com>
2022-02-09 11:03:25 -0300

    Exercicios modulo 3

---
+++ modulo_3/exercicio1.py
@@ -0,0 +1,12 @@
+from pickle import FALSE, TRUE
+
+
+nome        = "Leandro"
+idade       = 25
+altura      = 1.6
+estudante   = True
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(estudante))
