249e0bb867fff27977a91a10f4db97a1ee726a49
Leandro Watanabe <setsuo.leandro@gmail.com>
2022-02-09 11:44:28 -0300

    Revert "Exercicios modulo 3"
    This reverts commit c9961424b4e8a4e1999f1ad58442dfe7860a9f92.

--- modulo_3/exercicio2.py
+++
@@ -1,9 +0,0 @@
-valor_compras       = int(input())
-desconto            = float(input())
-quantidade_itens    = int(input())
-
-valor_final = valor_compras*(1-desconto)
-custo_medio = valor_compras/quantidade_itens
-
-print("Valor final: ", valor_final)
-print("Valor medio: ", custo_medio)
